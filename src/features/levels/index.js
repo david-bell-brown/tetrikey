90c9130f985877e9be5576e90b5c42fa007c8f17
David Bell-Brown <david.bellbrown@gmail.com>
2021-06-13 05:39:59 -0700

    more level progress

--- src/features/levels/index.js
+++ src/features/levels/index.js
@@ -2,6 +2,8 @@ import level1 from './level_1.json'
 import level2 from './level_2.json'
 import level3 from './level_3.json'
 import level4 from './level_4.json'
+import level5 from './level_5.json'
+import level6 from './level_6.json'
 import parseLevel from './parseLevel'
 
 const levels = ([
@@ -9,6 +11,8 @@ const levels = ([
   level2,
   level3,
   level4,
+  level5,
+  level6,
 ]).map(level => parseLevel(level))
 
 export default levels
